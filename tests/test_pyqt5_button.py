f86140bf2256dcb315387e5e13ebb5fd9f556f63
Samuel <contact@samuelkrieg.com>
2019-03-15 02:33:19 +0100

    added gui test

---
+++ tests/test_pyqt5_button.py
@@ -0,0 +1,15 @@
+import unittest
+
+from PyQt5.QtCore import *
+
+from src import button
+
+
+def test_hello(qtbot):
+    widget = button.SimpleButton()
+    qtbot.addWidget(widget)
+
+    # click in the Greet button and make sure it updates the appropriate label
+    qtbot.mouseClick(widget.buttonA, Qt.LeftButton)
+
+    assert widget.labelA.text() == "Changed"
